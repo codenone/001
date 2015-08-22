fdcdd0cb3bd58efc28b263c88b0346d00946defd
renatolima92 <renato.lima92@gmail.com>
2015-08-22 15:42:03 -0300

    Menu Inicial e Primeira Fase Agora o projeto é Standalone

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    #region external-methods
+    public void LoadLevel(int level)
+    {
+        Application.LoadLevel(level);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
--- Assets/Standard Assets/Utility/ForcedReset.cs
+++ Assets/Standard Assets/Utility/ForcedReset.cs
@@ -1,6 +1,5 @@
 using System;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
 
 [RequireComponent(typeof (GUITexture))]
 public class ForcedReset : MonoBehaviour
@@ -8,7 +7,7 @@ public class ForcedReset : MonoBehaviour
     private void Update()
     {
         // if we have forced a reset ...
-        if (CrossPlatformInputManager.GetButtonDown("ResetObject"))
+        if (Input.GetButtonDown("ResetObject"))
         {
             //... reload the scene
             Application.LoadLevelAsync(Application.loadedLevelName);
